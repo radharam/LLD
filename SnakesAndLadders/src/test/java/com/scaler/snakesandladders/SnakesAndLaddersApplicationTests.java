3e402e69dc97a5ccadf4fb4f5c2306a0a4f2307b
Radha <rkunnattur@gmail.com>
2022-07-27 17:16:04 +0530

    commit changes

---
+++ SnakesAndLadders/src/test/java/com/scaler/snakesandladders/SnakesAndLaddersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.snakesandladders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakesAndLaddersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
